d181cb135e5f3b0d248d08708955ff60697c3077
Chris Arakelian <coolacpc@gmail.com>
2020-07-25 01:47:07 +0300

    Updates

---
+++ precache-manifest.336288c2121a36ef06dcd181281bd3f5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667d4f97328530eedca3bf1ebf2008d8",
+    "url": "/zenhairshop/index.html"
+  },
+  {
+    "revision": "2d2c0dc51d5633b198ab",
+    "url": "/zenhairshop/static/css/2.7edfe34c.chunk.css"
+  },
+  {
+    "revision": "9f8b7cae728994ab49e0",
+    "url": "/zenhairshop/static/css/main.b75e00db.chunk.css"
+  },
+  {
+    "revision": "2d2c0dc51d5633b198ab",
+    "url": "/zenhairshop/static/js/2.4f96f285.chunk.js"
+  },
+  {
+    "revision": "9786fc046578b58583b817f48157ddeb",
+    "url": "/zenhairshop/static/js/2.4f96f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8b7cae728994ab49e0",
+    "url": "/zenhairshop/static/js/main.f5f5477f.chunk.js"
+  },
+  {
+    "revision": "e080e2211967b7ea5928",
+    "url": "/zenhairshop/static/js/runtime-main.b25858e2.js"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/zenhairshop/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "1a427fced5c79dda775bcb8769b212f0",
+    "url": "/zenhairshop/static/media/en.1a427fce.png"
+  },
+  {
+    "revision": "1a42036ee87c72fb329e662d689063d7",
+    "url": "/zenhairshop/static/media/girl.1a42036e.png"
+  },
+  {
+    "revision": "43498ef85c19a071fc3c88930bb6150d",
+    "url": "/zenhairshop/static/media/girl2.43498ef8.png"
+  },
+  {
+    "revision": "19dacf63d296ae2a0853fa822104e78a",
+    "url": "/zenhairshop/static/media/logo.19dacf63.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/zenhairshop/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/zenhairshop/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/zenhairshop/static/media/revicons.2feb69cc.eot"
+  }
+]);
